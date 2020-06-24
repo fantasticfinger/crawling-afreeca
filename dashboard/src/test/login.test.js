86adf34daabeaf28aa6fd3fb379a1acdfa1c562d
psh <keep4404@gamil.com>
2020-06-24 17:09:40 +0900

    login test refactoring

--- dashboard/src/test/login.test.js
+++ dashboard/src/test/login.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, fireEvent, act } from '@testing-library/react';
-import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import LoginComponent from 'components/auths/login';
 import { BaseProvider } from 'contexts/BaseContext';
@@ -14,25 +13,35 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe('<LoginComponent/>', () => {
-  it('inputs render', () => {
-    const { getByPlaceholderText, getByText } = render(
+  function setUp() {
+    const utils = render(
       <BaseProvider>
         <LoginComponent />
       </BaseProvider>,
     );
-    getByPlaceholderText('아이디');
-    getByPlaceholderText('비밀번호');
-    getByText('로그인');
-    getByText('회원가입');
-  });
-  it('change input', () => {
-    const { getByPlaceholderText } = render(
-      <BaseProvider>
-        <LoginComponent />
-      </BaseProvider>,
-    );
+    const { getByPlaceholderText, getByText } = utils;
     const idInput = getByPlaceholderText('아이디');
     const passwordInput = getByPlaceholderText('비밀번호');
+    const loginButton = getByText('로그인');
+    const signButton = getByText('회원가입');
+
+    return {
+      ...utils,
+      idInput,
+      passwordInput,
+      loginButton,
+      signButton,
+    };
+  }
+  it('inputs render', () => {
+    const { idInput, passwordInput, loginButton, signButton } = setUp();
+    expect(idInput).toBeTruthy();
+    expect(passwordInput).toBeTruthy();
+    expect(loginButton).toBeTruthy();
+    expect(signButton).toBeTruthy();
+  });
+  it('change input', () => {
+    const { idInput, passwordInput } = setUp();
 
     fireEvent.change(idInput, {
       target: {
@@ -50,13 +59,7 @@ describe('<LoginComponent/>', () => {
 
   it('click login', async () => {
     axios.post = jest.fn();
-    const { getByText, getByPlaceholderText } = render(
-      <BaseProvider>
-        <LoginComponent />
-      </BaseProvider>,
-    );
-    const idInput = getByPlaceholderText('아이디');
-    const loginButton = getByText('로그인');
+    const { idInput, loginButton } = setUp();
     await act(async () => {
       fireEvent.click(loginButton);
       fireEvent.change(idInput, {
@@ -68,13 +71,8 @@ describe('<LoginComponent/>', () => {
     expect(axios.post).toBeCalledTimes(1);
   });
   it('click sign', async () => {
-    const { getByText } = render(
-      <BaseProvider>
-        <LoginComponent />
-      </BaseProvider>,
-    );
+    const { signButton } = setUp();
 
-    const signButton = getByText('회원가입');
     await act(async () => {
       fireEvent.click(signButton);
     });
