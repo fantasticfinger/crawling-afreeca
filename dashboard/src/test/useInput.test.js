e11c49854e5261b9ce4aa0ee88f52ac4759f42fb
psh <keep4404@gamil.com>
2020-06-24 15:14:39 +0900

    useInput test

---
+++ dashboard/src/test/useInput.test.js
@@ -0,0 +1,39 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import useInput from '../hooks/useInput'
+
+test('useInput', async () => {
+    const initState = {
+        name: '',
+        password: '',
+    }
+    const { result } = renderHook(() => useInput(initState));
+    const event = {
+        target: {
+            name: 'name',
+            value: 'test'
+        }
+    }
+    const [, onChange, reset] = result.current
+    act(() => {
+        onChange({
+            target: {
+                name: 'name',
+                value: 'test'
+            }
+        });
+        onChange({
+            target: {
+                name: 'password',
+                value: '1234'
+            }
+        });
+    })
+
+    expect(result.current[0].name).toBe('test')
+    expect(result.current[0].password).toBe('1234')
+    act(() => {
+        reset();
+    })
+    expect(result.current[0].name).toBe('')
+    expect(result.current[0].password).toBe('')
+})
