89ffd682b87d67dbdd021de3d889f55187cc007f
psh <keep4404@gamil.com>
2020-06-24 14:42:11 +0900

    baseContest checkoutLogin test

--- dashboard/src/test/baseContext.test.js
+++ dashboard/src/test/baseContext.test.js
@@ -1,11 +1,12 @@
 import { renderHook, act } from '@testing-library/react-hooks';
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import {
   BaseProvider,
   useRegisterState,
   useRegisterDispath,
   login,
+  checkLogin,
 } from '../contexts/BaseContext';
 
 jest.mock('axios');
@@ -16,10 +17,13 @@ const useTest = () => {
   const loginTest = (name, password) => {
     login(dispatch, { name, password });
   };
-  return { state, loginTest };
+  const checkLoginTest = () => {
+    checkLogin(dispatch);
+  };
+  return { state, loginTest, checkLoginTest };
 };
 
-test('test', async () => {
+test('test login', async () => {
   const wrapper = ({ children }) => <BaseProvider>{children}</BaseProvider>;
   const { result, waitForNextUpdate } = renderHook(() => useTest(), {
     wrapper,
@@ -33,6 +37,30 @@ test('test', async () => {
   });
   act(() => {
     result.current.loginTest('test', '1234');
+  });
+  await waitForNextUpdate();
+  const { data } = result.current.state.register;
+
+  expect(data.logged).toBe(true);
+  expect(data.name).toBe('test');
+  expect(data.authority).toBe(1);
+});
+
+test('test checkLogin', async () => {
+  const wrapper = ({ children }) => <BaseProvider>{children}</BaseProvider>;
+  const { result, waitForNextUpdate } = renderHook(() => useTest(), {
+    wrapper,
+  });
+  axios.get = jest.fn().mockResolvedValue({
+    data: {
+      logged: true,
+      authority: 1,
+      name: 'test',
+    },
+  });
+
+  act(() => {
+    result.current.checkLoginTest();
   });
   await waitForNextUpdate();
   const { data } = result.current.state.register;
